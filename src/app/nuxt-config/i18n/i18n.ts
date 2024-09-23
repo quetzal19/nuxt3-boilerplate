5381198a629a9574f5ebd257d650f1cd6373b6a6
Denis-Str <web_life@inbox.ru>
2024-09-23 13:02:34 +0300

    npm update от 23.11 package.json

--- src/app/nuxt-config/i18n/i18n.ts
+++ src/app/nuxt-config/i18n/i18n.ts
@@ -1,16 +1,15 @@
 // https://i18n.nuxtjs.org/docs/options
 export default {
-  // vueI18n: 'src/app/nuxt-config/i18n/i18n.config.ts',
   locales: [
     {
       code: 'ru',
-      iso: 'ru-Ru',
+      language: 'ru-Ru',
       file: 'ru/ru.ts',
       name: 'Рус',
     },
     {
       code: 'en',
-      iso: 'en-US',
+      language: 'en-US',
       file: 'en/en.ts',
       name: 'Eng',
     },
